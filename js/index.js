0ef0f1c020fd086137002fcceba3b74e6c79e45c
Rabin <proprogrammer186@gmail.com>
2021-09-14 07:37:19 +0545

    Tried to make a game with hyte help of js.

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 19;
+let speed =  10 ;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -45,7 +45,7 @@ function gameEngine(){
         gameOverSound.play();
         musicSound.pause();
         inputDir =  {x: 0, y: 0}; 
-        alert("Game Over. Press any key to play again!");
+        alert("Game Over. Press Enter key to play again!");
         snakeArr = [{x: 13, y: 15}];
         musicSound.play();
         score = 0; 
